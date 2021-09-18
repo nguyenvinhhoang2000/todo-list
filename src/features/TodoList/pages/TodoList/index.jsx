4384ecca89ae1bcab1e3f00d18a0367d1d74bb04
Vinh Hoang <nguyenvinhhoang2000@gmail.com>
2021-09-18 20:11:48 +0700

    update

---
+++ src/features/TodoList/pages/TodoList/index.jsx
@@ -0,0 +1,33 @@
+import React from "react";
+import PropTypes from "prop-types";
+
+TodoList.propTypes = {
+  todoList: PropTypes.array,
+  onTodoClick: PropTypes.func,
+};
+
+TodoList.defaultTypes = {
+  todoList: [],
+  onTodoClick: null,
+};
+
+function TodoList(props) {
+  const { todoList, onTodoClick } = props;
+  const handleTodoClick = (todo, index) => {
+    if (!onTodoClick) return;
+
+    onTodoClick(todo, index);
+  };
+
+  return (
+    <ul className='todo-list'>
+      {todoList.map((todo, index) => {
+        <li key={todo.id} onClick={() => handleTodoClick(todo, index)}>
+          {todo.title}
+        </li>;
+      })}
+    </ul>
+  );
+}
+
+export default TodoList;
