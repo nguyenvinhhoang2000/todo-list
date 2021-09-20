576393e73cda3a73a90df44ef107f3e2064eef65
Vinh Hoang <nguyenvinhhoang2000@gmail.com>
2021-09-20 17:10:53 +0700

    update todo delete-filter

--- src/features/TodoList/components/TodoList/index.jsx
+++ src/features/TodoList/components/TodoList/index.jsx
@@ -1,29 +1,59 @@
 import React from "react";
 import PropTypes from "prop-types";
+import classNames from "classnames";
+import "./style.scss";
+import IconButton from "@mui/material/IconButton";
+import Tooltip from "@mui/material/Tooltip";
+import DeleteOutlineIcon from "@mui/icons-material/DeleteOutline";
 
 TodoList.propTypes = {
   todoList: PropTypes.array,
   onTodoClick: PropTypes.func,
+  onTodoClickDelete: PropTypes.func,
 };
 
 TodoList.defaultTypes = {
   todoList: [],
   onTodoClick: null,
+  onTodoClickDelete: null,
 };
 
 function TodoList(props) {
-  const { todoList, onTodoClick } = props;
+  const { todoList, onTodoClick, onTodoClickDelete } = props;
   const handleTodoClick = (todo, index) => {
     if (!onTodoClick) return;
 
     onTodoClick(todo, index);
   };
 
+  const handleTodoClickDelete = (todo, index) => {
+    if (!onTodoClickDelete) return;
+
+    onTodoClickDelete(todo, index);
+  };
+
   return (
     <ul className='todo-list'>
       {todoList.map((todo, index) => (
-        <li key={todo.id} onClick={() => handleTodoClick(todo, index)}>
-          {todo.title}
+        <li className='todo-item' key={todo.id}>
+          <p
+            className={classNames({
+              completed: todo.status === "completed",
+            })}
+            key={todo.id}
+            onClick={() => handleTodoClick(todo, index)}
+          >
+            {todo.title}
+          </p>
+
+          <Tooltip
+            onClick={() => handleTodoClickDelete(todo, index)}
+            title='Delete'
+          >
+            <IconButton>
+              <DeleteOutlineIcon />
+            </IconButton>
+          </Tooltip>
         </li>
       ))}
     </ul>
--- src/features/TodoList/pages/ListPage/index.jsx
+++ src/features/TodoList/pages/ListPage/index.jsx
@@ -1,44 +1,101 @@
 import React, { useState } from "react";
 import TodoForm from "../../components/TodoForm";
 import TodoList from "../../components/TodoList";
+import "./style.scss";
 
 function ListPage() {
   const initTodoList = [
     {
       id: 1,
       title: "Eat",
+      status: "completed",
     },
     {
       id: 2,
       title: "Sleep",
+      status: "new",
     },
     {
       id: 3,
       title: "Code",
+      status: "completed",
     },
     {
       id: 4,
       title: "Play Game",
+      status: "new",
     },
     {
       id: 5,
       title: "Learn ReactJS",
+      status: "new",
     },
     {
       id: 6,
       title: "Learn NodeJs",
+      status: "completed",
     },
   ];
 
   const [todoList, setTodoList] = useState(initTodoList);
+  const [filteredStatus, setFilteredStatus] = useState("all");
+
   const handleTodoSubmit = (values) => {
     const newTodo = {
       id: todoList.length + 1,
       title: values.title,
+      status: "new",
     };
 
     const newTodoList = [...todoList, newTodo];
     setTodoList(newTodoList);
+  };
+
+  const handleTodoClick = (todo, index) => {
+    const newTodoList = [...todoList];
+
+    newTodoList[index] = {
+      ...newTodoList[index],
+      status: newTodoList[index].status === "new" ? "completed" : "new",
+    };
+    setTodoList(newTodoList);
+  };
+
+  const handleTodoClickDelete = (todo, index) => {
+    // const indexTodo = todoList.findIndex((x) => x.id === todo.id);
+
+    // const newTodoList = [...todoList];
+    // console.log(newTodoList);
+    // newTodoList.splice(indexTodo, 1);
+    // console.log(newTodoList);
+    // setTodoList(newTodoList);
+
+    if (index < 0) return;
+
+    const newTodo = [...todoList];
+    newTodo.splice(index, 1);
+    setTodoList(newTodo);
+  };
+
+  //-----------fitter All New Completed Clear-----------------
+  const handleAllClick = () => {
+    setFilteredStatus("all");
+  };
+
+  const handleNewClick = () => {
+    setFilteredStatus("new");
+  };
+
+  const handleCompletedClick = () => {
+    setFilteredStatus("completed");
+  };
+
+  const renderTodoList = todoList.filter(
+    (todo) => filteredStatus === "all" || filteredStatus === todo.status
+  );
+  const handleClearCompletedClick = () => {
+    const newTodoClear = todoList.filter((todo) => todo.status !== "completed");
+    setTodoList(newTodoClear);
   };
 
   return (
@@ -46,7 +103,31 @@ function ListPage() {
       <h1>Todo List</h1>
       <TodoForm onSubmit={handleTodoSubmit} />
 
-      <TodoList todoList={todoList} />
+      <TodoList
+        todoList={renderTodoList}
+        onTodoClick={handleTodoClick}
+        onTodoClickDelete={handleTodoClickDelete}
+      />
+      <li className='todo-bottom'>
+        <p className='todo-bottom--left'>{todoList.length} item left</p>
+        <div className='todo-bottom--mid'>
+          <button className='todo-bottom--right' onClick={handleAllClick}>
+            All
+          </button>
+          <button className='todo-bottom--right' onClick={handleNewClick}>
+            New
+          </button>
+          <button className='todo-bottom--right' onClick={handleCompletedClick}>
+            Completed
+          </button>
+        </div>
+        <button
+          onClick={handleClearCompletedClick}
+          className='todo-bottom--right'
+        >
+          Clear Completed
+        </button>
+      </li>
     </div>
   );
 }
